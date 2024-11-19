1be541988f0507ff640f91b0efbc7cb416c6ef3e
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2024-11-19 17:32:02 +0100

    Mise à jour du portfolio

---
+++ projet/projet1/script.js
@@ -0,0 +1,7 @@
+document.getElementById("play-button").addEventListener("click", function() {
+    document.location.href = 'https://enzochatellard.alwaysdata.net/game/';
+});
+
+document.getElementById("back").addEventListener("click", function() {
+    document.location.href = '../../index.html';
+});
--- script.js
+++ script.js
@@ -9,3 +9,42 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 1000);
     });
 });
+
+function toggleDetails(btn) {
+    var details = btn.parentElement.nextElementSibling; // Trouve le div ".details" associé
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+        btn.textContent = '- Masquer les détails';
+    } else {
+        details.style.display = 'none';
+        btn.textContent = '+ Afficher les détails';
+    }
+}
+
+document.getElementById("Synthèse").addEventListener("click", function() {
+    const pdfUrl = 'projects_picture/Tableau_Synthèse.pdf';
+    
+    // Créer un lien temporaire pour le téléchargement
+    const link = document.createElement("a");
+    link.href = pdfUrl;  // L'URL du PDF à télécharger
+    link.download = 'Tableau_Synthèse.pdf';  // Nom du fichier lors du téléchargement
+    
+    // Simuler un clic sur le lien pour déclencher le téléchargement
+    link.click();
+});
+
+document.getElementById("CV").addEventListener("click", function() {
+    const pdfUrl = 'projects_picture/CV_Ouadah_Sofiane_Alternance.pdf';
+    
+    // Créer un lien temporaire pour le téléchargement
+    const link = document.createElement("a");
+    link.href = pdfUrl;  // L'URL du PDF à télécharger
+    link.download = 'CV_Ouadah_Sofiane_Alternance.pdf';  // Nom du fichier lors du téléchargement
+    
+    // Simuler un clic sur le lien pour déclencher le téléchargement
+    link.click();
+});
+
+function openPDF(pdfFile) {
+    window.open(pdfFile, '_blank');
+}
