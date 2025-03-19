a46f0b8501184c36e88e4b8e64a31eb3c6ed8b11
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2025-03-19 12:28:17 +0100

    Ajout de la doc technique

--- projet/projet6/script.js
+++ projet/projet6/script.js
@@ -6,9 +6,15 @@ function openPDF(pdfUrl) {
         alert('Impossible d\'ouvrir le PDF. Vérifiez les paramètres de votre navigateur.');
     }
 }
-document.getElementById("daily").addEventListener("click", function() {
+document.getElementById("dailyclient").addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = 'doc_daily/daily.pdf';
-    link.download = 'Documentation_dailyho.pdf';
+    link.href = 'doc_daily/dailyclient.pdf';
+    link.download = 'Documentation__client_dailyho.pdf';
+    link.click();
+});
+document.getElementById("dailyclient").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = 'doc_daily/dailytechnique.pdf';
+    link.download = 'Documentation_technique_dailyho.pdf';
     link.click();
 });
