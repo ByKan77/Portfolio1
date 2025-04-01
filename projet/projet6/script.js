baaf6e65a42a4244e48d640d353ea79916851189
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2025-04-01 15:03:48 +0200

    Changement dernière minute

--- projet/projet6/script.js
+++ projet/projet6/script.js
@@ -6,15 +6,30 @@ function openPDF(pdfUrl) {
         alert('Impossible d\'ouvrir le PDF. Vérifiez les paramètres de votre navigateur.');
     }
 }
-document.getElementById("dailyclient").addEventListener("click", function() {
+document.getElementById("dailyClientBtn").addEventListener("click", function() {
     const link = document.createElement("a");
     link.href = 'doc_daily/dailyclient.pdf';
-    link.download = 'Documentation__client_dailyho.pdf';
+    link.download = 'Documentation_client_dailyho.pdf';
     link.click();
 });
-document.getElementById("dailyclient").addEventListener("click", function() {
+
+document.getElementById("dailyTechBtn").addEventListener("click", function() {
     const link = document.createElement("a");
     link.href = 'doc_daily/dailytechnique.pdf';
     link.download = 'Documentation_technique_dailyho.pdf';
+    link.click();
+});
+
+document.getElementById("appClientBtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = 'doc_daily/dailyapp.pdf';
+    link.download = 'Documentation_application_client_dailyho.pdf';
+    link.click();
+});
+
+document.getElementById("appTechBtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = 'doc_daily/dailyapp.pdf';
+    link.download = 'Documentation_application_technique_dailyho.pdf';
     link.click();
 });
